af045c0eb77ed626d1973f1815ee17cd0221e15f
Abdul Mughni <pro@192.168.1.20>
2023-10-12 13:24:20 +0500

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: 'Airpods Pro',
+    image: '/images/airpods2.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'iPhone 15 Pro Max 256GB Memory',
+    image: '/images/iphone.png',
+    description:
+      'Introducing the iPhone 15 Pro Max. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life.New titanium Body with type c and an action button',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera2.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 5 Pro',
+    image: '/images/ps.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 10,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple Wireless Blutooth Mouse',
+    image: '/images/applemouse.jpg',
+    description:
+      'Get a better handle on your games with this Apple Mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple Wireless Battery Pack',
+    image: '/images/magsafe.jpg',
+    description:
+      'The Apple Battery Pack is an accessory designed and produced by Apple Inc. It is a portable power bank specifically created for use with Apple devices, such as iPhones. The battery pack is designed to provide additional battery life to your Apple device, allowing you to stay connected and use your device for longer periods  ',
+    brand: 'Apple',
+    category: 'Accessories',
+    price: 29.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple Watch series 9',
+    image: '/images/watchs9.jpg',
+    description:
+      'The Apple Watch is a highly popular smartwatch developed by Apple Inc. It combines a stylish design with advanced technology, offering users a range of features such as fitness tracking, heart rate monitoring, notifications, and access to a wide variety of apps',
+    brand: 'Apple',
+    category: 'Accessories',
+    price: 429.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple 20W Power Adapter',
+    image: '/images/charger.jpg',
+    description:
+      'The Apple Power Adapter is an accessory designed and produced by Apple Inc. It is a portable power bank specifically created for use with Apple devices, such as iPhones. The battery pack is designed to provide additional battery life to your Apple device, allowing you to stay connected and use your device for longer periods  ',
+    brand: 'Apple',
+    category: 'Accessories',
+    price: 29.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default products
